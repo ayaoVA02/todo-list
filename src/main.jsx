cb39cb9d3fa0f4cff2edc156355950766f4a20c6
unknown <ayaoVA02>
2026-03-22 21:56:24 +0700

    fix no found route

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/todo_list'>
+    <BrowserRouter basename='/todo-list'>
       <App />
     </BrowserRouter>
   </StrictMode>,
